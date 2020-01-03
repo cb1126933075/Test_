629e4d35e77eb565608ae51b0914e7750d6a3fa7
Administrator <Administrator@JQJLJDG6B4V3H09>
2020-01-03 10:36:33 +0800

    2020  -  测试代码

---
+++ _test/src/_test/Test_.java
@@ -0,0 +1,12 @@
+package _test;
+/**
+* @author Chen Biao
+* @version 创建时间：2020年1月3日 上午10:29:53
+* @ClassName 类名称
+* @Description 类描述
+*/
+public class Test_ {
+	public static void main(String[] args) {
+		System.out.println("Hello 2020!");
+	}
+}	
